d7a015d255da02f6aff407c5fb52e74cc42b8c49
Omar Alvarez <omajul2@hotmail.com>
2016-05-23 20:26:48 +0200

    Finish step 8 - Creating (POST)

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,22 @@
 class RestaurantsController < ApplicationController
+	# Utility methods
+	def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+	
+	# GET
 	def index
 		@restaurants = Restaurant.all
+  end
+
+  # GET
+  def new
+  	@restaurant = Restaurant.new
+  end
+
+  # POST
+  def create
+  	Restaurant.create(restaurant_params)
+  	redirect_to '/restaurants'
   end
 end
