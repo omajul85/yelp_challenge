7d1cd4d270977030f775c68fe7569f85eeb27b30
Omar Alvarez <omajul2@hotmail.com>
2016-05-24 10:14:58 +0200

    WIP - Reviewing

---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Reviewing:' do
+	before { Restaurant.create(name: 'KFC') }
+
+	scenario 'allow users to leave review using a form' do
+		visit '/restaurants'
+		click_link 'Review'
+		fill_in 'Thouhts', with: "so so"
+		select '3', from: 'Rating'
+		click_button 'Leave Review'
+		
+		expect(current_path).to eq '/restaurants'
+		expect(page).to have_content("so so")
+	end
+
+end
+
